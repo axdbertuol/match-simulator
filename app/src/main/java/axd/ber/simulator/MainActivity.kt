8853b518a0c237fb5320de22b54eafbbe1f58bf9
axdr3 <axdber@gmail.com>
2022-01-19 05:09:43 -0300

    release:0.1.0-1

--- app/src/main/java/axd/ber/simulator/MainActivity.kt
+++ app/src/main/java/axd/ber/simulator/MainActivity.kt
@@ -2,10 +2,15 @@ package axd.ber.simulator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import axd.ber.simulator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // view binding config
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
